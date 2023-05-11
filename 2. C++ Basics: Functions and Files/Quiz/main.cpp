7c4ae633c67ad2051a5836546d394ff7667082fb
Jack Howells <98927631+jh2952@users.noreply.github.com>
2023-05-11 22:43:07 +0100

    Update main.cpp

--- 2. C++ Basics: Functions and Files/Quiz/main.cpp
+++ 2. C++ Basics: Functions and Files/Quiz/main.cpp
@@ -1,3 +1,7 @@
+// 1. Write a program (named main.cpp) that reads two seperate integers from the user, adds them together, and then outputs the answer.
+// 2. Contain readNumber() and writeAnswer() in a seperate file called "io.cpp".
+// 3. Write a header file "io.h" to access the functions instead of using forward declerations in main.
+
 #include <iostream>
 #include "io.h"
 
