0a422cec0dc73bf84ed4ba99f1f415b35b52f0dc
Jack Howells <98927631+jh2952@users.noreply.github.com>
2023-05-11 22:40:43 +0100

    Update Q3.cpp

--- 1. C++ Basics/Q3.cpp
+++ 1. C++ Basics/Q3.cpp
@@ -1,3 +1,6 @@
+// Write a program that asks the user to enter a number, and then enter a second number.
+// The program should tell the user what the result of adding and subtracting the two numbers is.
+
 #include <iostream>
 
 int main() // Recives two integers from input and outputs sum and difference.
